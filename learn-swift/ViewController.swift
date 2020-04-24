db620d93ff65363fe1e60149eb582b0d0002936b
kerami <keramiozsoy@hotmail.com>
2020-04-24 12:40:40 +0300

    lesson 4

--- learn-swift/ViewController.swift
+++ learn-swift/ViewController.swift
@@ -15,13 +15,32 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
 
         
-        let deger:UInt = 100;
+        // switch
+        let sonuc:UInt = 100;
 
-        if deger == 100 {
-            print("hello");
-        } else {
-            print("world");
+        switch sonuc{
+            case 1:
+                print("11");
+            case 2...14:
+                print("144");
+            default:
+                print("yanlis cevap");
         }
+        
+        
+        // switch where
+        let yetAnotherPoint = (1, -1);
+        
+        switch yetAnotherPoint {
+        case let (x, y) where x == y:
+            print("(\(x), \(y)) is on the line x == y")
+        case let (x, y) where x == -y:
+            print("(\(x), \(y)) is on the line x == -y")
+        case let (x, y):
+            print("(\(x), \(y)) is just some arbitrary point")
+        }
+        
+        
 
     }
 
